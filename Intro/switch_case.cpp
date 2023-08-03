ef71ad06b9880966b016a6f9b48b9c0fef1c7950
indra-jith <indrajithmp77@gmail.com>
2023-08-03 20:11:23 +0530

    for loop

--- Intro/switch_case.cpp
+++ Intro/switch_case.cpp
@@ -9,14 +9,19 @@ int main()
     {
         case 1 : cout << "You entered number 1" << endl;
         break;
+
         case 2 : cout <<"You entered number 2" <<endl;
         break;
+
         case 3 : cout <<"You entered number 3" <<endl;
         break;
+
         case 4 : cout <<"You entered number 4" <<endl;
         break;
+
         case 5 : cout <<"You entered number 5" <<endl;
         break;
+        
         default: cout <<"The number is not in the range from 1 to 5" <<endl;
         break;
     }
