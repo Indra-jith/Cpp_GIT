8618b25917c78f822e210153243d0bed4103cd21
indra-jith <indrajithmp77@gmail.com>
2023-08-04 20:01:34 +0530

    patterns

---
+++ Intro/pattern_printing.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int row,col;
+    // cout<<"Enter the number of rows and columns: "<<endl;
+    // cin>>row;
+    // cin>>col;
+    // for(int i=1;i<=row;i++)
+    // {
+    //     for(int j=1;j<=col;j++)
+    //     {
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int row,col;
+    // cout<<"Enter the number of rows and columns: "<<endl;
+    // cin>>row;
+    // cin>>col;
+    // for(int i=1;i<=row;i++)
+    // {
+    //     for(int j=row;j>=1;j--)
+    //     {
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+
+    // //using while loop
+    //     int n;
+    //     cin>>n;
+
+    //     int row=1;
+    //     while(row<=n)
+    //     {
+    //         int col=1;
+    //         while(col<=n){
+    //             cout<<col;
+    //             col++;
+    //         }
+    //         cout<<endl;
+    //         row++;
+    //     }
+    // }
+
+    // for(int i=1;i<=5;i++)
+    // {
+    //     for(int j=1;j<=i;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+}
