ea316e57c3636bc96d6539a035e26bafcb3c9dfe
indra-jith <indrajithmp77@gmail.com>
2023-08-07 19:27:10 +0530

    local global

---
+++ Intro/local_global.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int glovbalVar = 10; //global
+void functionn()
+{
+    cout<<"4";
+    int localvar = 20; //restricted
+}
+int main()
+{
+    cout<<"Hello!";
+}
