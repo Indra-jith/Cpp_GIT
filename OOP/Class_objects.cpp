fc8d4429216a99d20cb1014d64899b5e4797a938
indra-jith <indrajithmp77@gmail.com>
2023-08-08 20:35:32 +0530

    access modifiers

--- OOP/Class_objects.cpp
+++ OOP/Class_objects.cpp
@@ -4,10 +4,12 @@ using namespace std;
 //Access modifiers
 
 class Building{
+    //by default -> private
     //attributes
-    int door;
+    int door; //private by default
     string doorColor;
-    int window;
+    public:
+    int window; // from this it won't be accessible outside the class
     string windowColor;
     int elevator;
 
@@ -18,5 +20,7 @@ class Building{
 };
 int main()
 {
-
+    Building b1;
+    b1.window; //ok
+    //b1.door not acccessible, error
 }
