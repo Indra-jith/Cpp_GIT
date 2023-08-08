c13df649c871a7bd63518fd285c4d38432b0d61c
indra-jith <indrajithmp77@gmail.com>
2023-08-08 20:15:46 +0530

    oops

---
+++ OOP/Class_objects.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+//Access modifiers
+
+class Building{
+    //attributes
+    int door;
+    string doorColor;
+    int window;
+    string windowColor;
+    int elevator;
+
+    //behavior
+    void elevatorFunctioning(){
+        //working of elevator
+    }
+};
+int main()
+{
+
+}
