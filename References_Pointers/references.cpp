fc2665bcc2150aa0fbbe2ecc6c1d01c835d6efa6
indra-jith <indrajithmp77@gmail.com>
2023-08-21 19:45:13 +0530

    reference

---
+++ References_Pointers/references.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//Reference Variable
+//It is a reference to an existing variable
+
+int main()
+{
+    // string name = "Hello";
+    // string &ref = name;
+
+    // & symbol -> indicates that it is reference
+
+    // cout << name << endl; //both will give
+    // cout<< ref ; //same result
+
+    // int a = 5;
+    // int &b = a;
+    // cout << a << endl; //5
+    // a++;
+    // cout << a << endl; //6
+    // b++;
+    // cout << a << endl; //7 (doesn't matter if b is incremented)
+
+    
+}
