d819db33d5af6b5c1283b1001872947fdaf80c78
indra-jith <indrajithmp77@gmail.com>
2023-08-23 20:19:38 +0530

    types of pointers

--- References_Pointers/Types_of_pointers.cpp
+++ References_Pointers/Types_of_pointers.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-// NULL POINTERS
+//1.  NULL POINTERS
 // - Which is pointing to nothing, if we don't have the address to be assigned
 //   to a pointer, we can use NULL
     // int *p; //containing garbage value, not a good method
@@ -11,22 +11,41 @@ int main()
     // cout<<p; //0
 
 
-//Double pointers - we can create a pointer that in turn may point tp
+//2. DOUBLE POINTERS - we can create a pointer that in turn may point tp
 // data or another pointer
 
-int a = 5;
-int *p = &a;
-int **q = &p;
+// int a = 5;
+// int *p = &a;
+// int **q = &p;
 
-cout << &a <<endl;
-cout << p <<endl;
-cout << *q <<endl; //all 3 gives same stuff
-cout<<endl;
-cout<<&p<<endl;
-cout<<q<<endl;//these 2 gives same stuff
+// cout << &a <<endl;
+// cout << p <<endl;
+// cout << *q <<endl; //all 3 gives same stuff
+// cout<<endl;
+// cout<<&p<<endl;
+// cout<<q<<endl;//these 2 gives same stuff
 
-cout<<a<<endl;
-cout<<*p<<endl;
-cout<<**q<<endl; //these 3 gives same stuff - 5
+// cout<<a<<endl;
+// cout<<*p<<endl;
+// cout<<**q<<endl; //these 3 gives same stuff - 5
 
+//3. VOID POINTERS - generic pointer
+//It is a generic pointer and it has no associated type with it 
+//It can hold an address of any type.
+//IT cannot be dereferenced
+
+// double i = 5;
+// void *ptr = &i;
+
+
+//4. WILD POINTERS - when a pointer is declared but not initialised
+//They point at a random memory location
+// int a = 5;
+// int *p;
+// cout<<p<<endl; //garbage addresss
+// cout<<*p<<endl; //garbage value
+
+
+//5. DANGLING POINTERS
+// A pointer towards a memory location that has been deleted.
 }
