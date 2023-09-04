1a295954e4c97ee75fc6b5e416d4f47c875e5464
indra-jith <indrajithmp77@gmail.com>
2023-09-04 18:43:07 +0530

    fibonacci problem

---
+++ Problems/prob.cpp
@@ -0,0 +1,43 @@
+// Given a staircase of N steps and you can either climb 1 or 2 steps at a given time. The task is to return the count of distinct ways to climb to the top.
+// Note: The order of the steps taken matters.
+
+// Examples:
+
+// Input: N = 3
+// Output: 3
+// Explanation:
+
+// There are three distinct ways of climbing a staircase of 3 steps :
+
+// [1, 1, 1], [2, 1] and [1, 2].
+
+
+// Another axample
+// Input: N = 2
+// Output: 2
+// Explanation:
+
+// There are two distinct ways of climbing a staircase of 3 steps :
+
+// [1, 1] and [2].
+
+//--------------------------------------------------------------------
+                    // SPOILER ALERT -> IT IS FIBONACCI //
+#include<iostream>
+using namespace std;
+
+int climbStair(int n)
+{
+    //base case
+    if(n < 2) return 1;
+
+    //recursive relation
+    
+    
+
+}
+
+int main()
+{
+
+}
