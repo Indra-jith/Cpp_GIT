3097da86c5f6eed411e8fd4c1f0910727649bd2f
indra-jith <indrajithmp77@gmail.com>
2023-09-07 19:18:54 +0530

    null character

---
+++ Extra/1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//NOTE->At the end of every string automatically '\0' is assigned
+// Null character
+//'\0' indicates that string has ended
+void printString(char* str)
+{
+
+}
+int main()
+{
+    char* myString = "Hello world";
+    printString(myString);
+}
