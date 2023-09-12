f9d1e05db9ae21fe506d519443839eeaaa310216
indra-jith <indrajithmp77@gmail.com>
2023-09-12 20:57:15 +0530

    operator overloading (!)

--- OOP/Operator_Overloading_EX2.cpp
+++ OOP/Operator_Overloading_EX2.cpp
@@ -1,7 +1,44 @@
+//NOT(!) WILL SWAP THE VALUES
+
 #include <iostream>
+#include<algorithm>
 using namespace std;
 
+class Shift
+{
+    int a;
+    int b;
+
+public:
+    Shift()
+    {
+        a=0;
+        b=0;
+    }
+
+    void setValues()
+    {
+        cout<<"Enter the first value: ";
+        cin>>a;
+        cout<<"Enter the second value: ";
+        cin>>b;
+    }
+
+    void operator !()
+    {
+        swap(a,b);
+    }
+
+    void displayValues()
+    {
+        cout<<"First value is "<<a<<endl;
+        cout<<"Second value is "<<b<<endl;
+    }
+};
 int main()
 {
-    
+    Shift s1;
+    s1.setValues();
+    !s1;
+    s1.displayValues();
 }
