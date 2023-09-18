7b1352f3cdd45978b373feea94a10f1bbe2953d8
indra-jith <indrajithmp77@gmail.com>
2023-09-18 20:05:53 +0530

    inheritance

--- OOP/Inheritance/Inheritance.cpp
+++ OOP/Inheritance/Inheritance.cpp
@@ -1,2 +1,38 @@
-#include<iostream>
+/*
+--Inheritance is a process in which one object acquires all the
+properties and behaviours of its parent object successfully.
+
+-- In such a way that we can reuse,extend or modify the attributes
+and behvaviors which are defined in the other class.
+
+-- Advantage - reusability
+
+*/
+
+#include <iostream>
 using namespace std;
+
+// class Base
+// {
+
+// };
+
+// class Derived : public Base // - - - > Syntax
+// {
+
+// };
+
+class Animal
+{
+
+};
+
+class Derived : public Animal // - - - > Syntax
+{
+    
+};
+
+int main()
+{
+
+}
