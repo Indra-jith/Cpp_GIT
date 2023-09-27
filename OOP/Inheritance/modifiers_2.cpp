26fb7872afd61fe458f5b9ad6a2264f78858f778
indra-jith <indrajithmp77@gmail.com>
2023-09-27 19:17:02 +0530

    modifiers

---
+++ OOP/Inheritance/modifiers_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+private:
+    int private1 = 1;
+
+protected:
+    int protected1 = 2;
+
+public:
+    int public1 = 3;
+
+};
+
+int main()
+{
+    
+}
