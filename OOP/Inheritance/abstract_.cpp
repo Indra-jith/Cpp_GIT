6d7ddb4ff539b7782c0baf2c62cfebfba1ab2223
indra-jith <indrajithmp77@gmail.com>
2023-10-16 19:20:46 +0530

    abstract class

---
+++ OOP/Inheritance/abstract_.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// abstract classes refers to a class containing atlease one pure virutal function,
+// which cannot be instantiated.
+
+class Shape{
+    
+};
+int main()
+{
+
+}
