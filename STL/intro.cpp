cfd0672d3652b6c852820382b52ac7a9eac50d46
indra-jith <indrajithmp77@gmail.com>
2023-10-17 20:41:30 +0530

    set intro

--- STL/intro.cpp
+++ STL/intro.cpp
@@ -7,12 +7,14 @@ using namespace std;
     1.Sequential container - it allows us to store elements that can be 
     accessed in sequential order eg- Array,List, Vector
 
-    2.Associative - It allows us to store elements in sorted order,
+    2.Associative containers - It allows us to store elements in sorted order,
     the order doesn't depend upon when the element is inserted.
     also removes the duplicates
     eg-set,map,multiset,multimap
 
-    3.Unordered Associative
+    3.Unordered Associative containers - It provides the unsorted version of
+    associative containers
+    eg - unordered set,
 */
 int main()
 {
@@ -22,10 +24,15 @@ int main()
     //     cout<<nums<<" "; //1,2,3,4,5
     // }
 
-    set<int> num = {1,9,10,8,7,10};
-    for (auto nums : num)
-    {
-        cout << nums << " "; //1,7,8,9,10 //avoids duplicates
-    }
+    // set<int> num = {1,9,10,8,7,10};
+    // for (auto nums : num)
+    // {
+    //     cout << nums << " "; //1,7,8,9,10 //avoids duplicates
+    // }
     
+    // unordered_set<int> num = {4,2,9,7,10,10};
+    // for (auto nums : num)
+    // {
+    //     cout << nums << " "; 
+    // }
 }
---
+++ STL/set.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+//Set - they store UNIQUE elemens of SAME TYPE in SORTED manner
+
+int main()
+{
+    
+}
