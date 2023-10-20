89902a5c0643f2f6bf8abc91e5afe6a9bec29403
indra-jith <indrajithmp77@gmail.com>
2023-10-20 19:56:02 +0530

    binary file handling

--- File_Handling/binary.cpp
+++ File_Handling/binary.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
 
 int main()
 {
+    //writing
+    // ofstream file ("test.bin", ios::binary);
+    // int p = 1234;
+    // int q = 5678;
+    // file.write((char*)&p,sizeof(p));
+    // file.write((char*)&q,sizeof(q));
 
+    ifstream file("test.bin",ios::binary);
+    int p;
+    file.read((char*)&p,sizeof(p));
+    cout<<"Value of p is: "<<p<<endl;
+    file.close();
 }
